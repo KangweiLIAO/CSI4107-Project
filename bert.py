62290865e19316c158fa8818228785580c8f5735
KangweiLIAO <clanceyliao@gmail.com>
2021-11-30 02:28:21 -0500

    BERT initialized

---
+++ bert.py
@@ -0,0 +1,9 @@
+import time
+import retrieval_utils as utils
+from retrieval_utils import CTColors
+from inverted_index import InvertedIndex
+from sentence_transformers import SentenceTransformer
+
+
+def similarity_scores(inv_index: InvertedIndex, queries: list[(str, list[str])]):
+	pass
