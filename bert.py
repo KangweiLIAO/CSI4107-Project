eca678b8a97c47be6367af2278caad5cc8bc9781
KangweiLIAO <clanceyliao@gmail.com>
2021-11-30 02:30:59 -0500

    BERT initialized

--- bert.py
+++ bert.py
@@ -4,6 +4,8 @@ from retrieval_utils import CTColors
 from inverted_index import InvertedIndex
 from sentence_transformers import SentenceTransformer
 
+model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+
 
 def similarity_scores(inv_index: InvertedIndex, queries: list[(str, list[str])]):
 	pass
