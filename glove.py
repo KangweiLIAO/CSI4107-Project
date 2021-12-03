023d04bd198c6e2cccd3654bc8d776510cb9ea7f
Aozhuo Zhang <47909752+MisakaZentai@users.noreply.github.com>
2021-12-02 20:04:06 -0500

    Update glove.py

--- glove.py
+++ glove.py
@@ -30,7 +30,7 @@ def output(train_data: dict[str, list[str]]):
 		print(s, file=data)
 
 
-def similarity_scores(inv_index: IRIndex, queries: list[(str, list[str])], topn=3, doc_per_query=1000):
+def similarity_scores(inv_index: IRIndex, queries: list[(str, list[str])], topn=1, doc_per_query=1000):
 	"""
 	Use the pre-trained glove model and do the query expansion. Finally return a ranked result for input queries on all documents
 	in ir_index.
@@ -57,7 +57,7 @@ def similarity_scores(inv_index: IRIndex, queries: list[(str, list[str])], topn=
 		q_terms += tmp
 
 		q_vec = get_mean_vector(model, q_terms)
-		for d_id, doc in inv_index.docs_dict.items():
+		for d_id, doc in inv_index.pred_docs_dict.items():
 			doc_avg_vec = get_mean_vector(model, doc)
 			if len(doc_avg_vec) > 0:
 				scores_dict[q_id].append((d_id, utils.np_cossim(q_vec, doc_avg_vec)))
@@ -70,7 +70,7 @@ def similarity_scores(inv_index: IRIndex, queries: list[(str, list[str])], topn=
 
 def get_mean_vector(glv_model, words):
 	"""
-	Calculate and return the mean vector of the words in given word2vec model.
+	Calculate and return the mean vector of the words in given glove model.
 
 	:param glv_model: trained glove model
 	:param words:
